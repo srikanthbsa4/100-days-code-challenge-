832d21383b32aa84a0b38fb21763bceb4d22756c
SRIKANTH B <125802352+srikanthbsa4@users.noreply.github.com>
2023-07-04 22:45:05 +0530

    Add files via upload

---
+++ sumcombinations.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2,num3;
+        printf("\n num1=");
+        scanf("%d",&num1);
+        printf("\n num2=");
+        scanf("%d",&num2);
+        printf("\n num3=");
+        scanf("%d",&num3);
+
+        printf("\n %d",num1+num2);
+        printf("\n %d",num2+num3);
+        printf("\n %d",num1+num3);
+
+}
+
