a680c6ee121786b1e06f875c9335c8ab9052e125
SRIKANTH B <125802352+srikanthbsa4@users.noreply.github.com>
2023-07-18 20:50:39 +0530

    Add files via upload

---
+++ Day26.c
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                printf("%d ",j+1); 
+            }
+            printf("\n");
+           
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
