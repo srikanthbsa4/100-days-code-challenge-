0b5de234985fcb84824eea7864022032a5de8306
SRIKANTH B <125802352+srikanthbsa4@users.noreply.github.com>
2023-07-18 21:06:33 +0530

    Add files via upload

---
+++ Day25.c
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+    int i,j;
+     for(j=0;j<n;j++)
+     {
+        for(i=0;i<n;i++)
+     {
+        printf("* ");
+     }
+        printf("\n");
+      
+     }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
