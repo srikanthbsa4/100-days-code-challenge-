5167b7565efa45ee8b6fe2ef32b79a88a5bb52f5
SRIKANTH B <125802352+srikanthbsa4@users.noreply.github.com>
2023-08-02 16:09:21 +0530

    Add files via upload

---
+++ Day37.c
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int N){
+        //code here
+        if(N==0)
+        return 1;
+        else
+        return N*factorial(N-1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
+
+
+
+int fib(int n){
+if(n==0 || n==1)
+return n;
+else
+return fib(n-1)+fib(n-2);
+
+}
